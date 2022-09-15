597f6f7aaee5ab4fe294a58b0ae90ae3c81c1804
Jean Janrley <jeanjanrley@outlook.com>
2022-09-15 17:21:20 -0300

    Circulo interativo quase finalizado

--- index.js
+++ index.js
@@ -27,74 +27,89 @@ buttons.forEach((element, index) => {
 });
 
 const bigCircle = document.querySelector("#big-circle");
+const interactiveMessage = document.querySelector("#big-circle > p");
 
 // Animação do circulo interativo
-const circlesId = {
-	top: "Grupo_126547",
-	left: "Grupo_126544",
-	right: "Grupo_126546"
-};
 
-const leftCircle = {
-	element: document.getElementById(circlesId.left),
+const redCircle = {
+	id: "Grupo_126544",
+	element: document.getElementById("Grupo_126544"),
 	color: "#FF3737",
 	message: "Texto referente ao botão que está sobre a flecha vermelha!",
-	textColor: ""
 };
-const rightCircle = {
-	element: document.getElementById(circlesId.top),
+const yellowCircle = {
+	id: "Grupo_126547",
+	element: document.getElementById("Grupo_126547"),
 	color: "#FFCC33",
 	message: "Este texto é referente ao botão que está sobre a flecha amarela!",
-	textColor: ""
 };
-const topCircle = {
-	element: document.getElementById(circlesId.right),
+const greyCircle = {
+	id: "Grupo_126546",
+	element: document.getElementById("Grupo_126546"),
 	color: "#707070",
 	message: "Aqui está sendo apresentado o texto do botão que está sobre a flecha cinza!",
-	textColor: ""
 };
+
+// Array de ciruclos
+const circles = [redCircle, greyCircle, yellowCircle];
 
 // icones de + e -
 const lessIcon = document.getElementById("Caminho_187625").cloneNode(true);
 const plusIcon = document.getElementById("Caminho_187624").cloneNode(true);
 
-function changeIcon({ element, color, message, textColor }) {
+function changeIcon({ element, color, message }) {
 	try {
-		const circle = element.childNodes[1].childNodes[1];
+		const circle = element.childNodes[1].childNodes[1]; // Refencia da do botão atual
+
 
 		// Lista de icones de +
 		const plusIconsIds = ["Caminho_187626", "Caminho_187624"];
 		const iconsIds = [plusIconsIds, "Caminho_187625"].flat();
 
 		// Limpa os filhos
-		const cleanNodes = () => {
-			element.childNodes.forEach((node, index) => {
+		const removeChilds = (item = element) => {
+			item.childNodes.forEach((node, index) => {
 				if (iconsIds.includes(node?.id)) {
-					element.removeChild(element.childNodes[index]);
+					item.removeChild(item.childNodes[index]);
 				}
 			});
 		};
+
+		// Resetar os botões para o estado positivo
+		circles.forEach(({ element: elementHere }) => {
+			console.log("element here: ", elementHere);
+			if (element?.id !== elementHere?.id) {
+				removeChilds(elementHere); // Remove todos os filhos do componente
+				elementHere.append(plusIcon.cloneNode(true)); // Adicionar o icone de  +
+				const circleHere = elementHere.childNodes[1].childNodes[1]; // Refencia da do botão atual
+				circleHere.setAttribute("fill", "#03B5B0"); // Adicionar a cor azul
+			}
+		});
 
 		// Verifica se o icone é de + ou de -
 		const nodes = [...element.childNodes];
 		const isPlusIcon = nodes.some(item => plusIconsIds.includes(item?.id));
 		if (isPlusIcon) {
-			cleanNodes();
+			removeChilds();
 			element.append(lessIcon.cloneNode(true));
 			circle.setAttribute("fill", "#7A2B58");
 		}
 		else {
-			cleanNodes();
+			removeChilds();
 			element.append(plusIcon.cloneNode(true));
 			circle.setAttribute("fill", "#03B5B0");
 		}
+
+		// Estilização do circulo central
 		bigCircle.setAttribute("style", `background: ${color}`);
+		interactiveMessage.innerHTML = message;
+		interactiveMessage.setAttribute("style", "color: #000");
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-leftCircle.element.addEventListener("click", () => changeIcon({ ...leftCircle }));
-topCircle.element.addEventListener("click", () => changeIcon({ ...topCircle }));
-rightCircle.element.addEventListener("click", () => changeIcon({ ...rightCircle }));
+redCircle.element.addEventListener("click", () => changeIcon({ ...redCircle }));
+greyCircle.element.addEventListener("click", () => changeIcon({ ...greyCircle }));
+yellowCircle.element.addEventListener("click", () => changeIcon({ ...yellowCircle }));
 
