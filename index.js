94df42b252e0f07d45533582100dff1c4341093f
Jean Janrley <jeanjanrley@outlook.com>
2022-09-15 16:46:18 -0300

    Refatoração de função dos botões

--- index.js
+++ index.js
@@ -26,5 +26,75 @@ buttons.forEach((element, index) => {
 	}
 });
 
+const bigCircle = document.querySelector("#big-circle");
+
 // Animação do circulo interativo
+const circlesId = {
+	top: "Grupo_126547",
+	left: "Grupo_126544",
+	right: "Grupo_126546"
+};
+
+const leftCircle = {
+	element: document.getElementById(circlesId.left),
+	color: "#FF3737",
+	message: "Texto referente ao botão que está sobre a flecha vermelha!",
+	textColor: ""
+};
+const rightCircle = {
+	element: document.getElementById(circlesId.top),
+	color: "#FFCC33",
+	message: "Este texto é referente ao botão que está sobre a flecha amarela!",
+	textColor: ""
+};
+const topCircle = {
+	element: document.getElementById(circlesId.right),
+	color: "#707070",
+	message: "Aqui está sendo apresentado o texto do botão que está sobre a flecha cinza!",
+	textColor: ""
+};
+
+// icones de + e -
+const lessIcon = document.getElementById("Caminho_187625").cloneNode(true);
+const plusIcon = document.getElementById("Caminho_187624").cloneNode(true);
+
+function changeIcon({ element, color, message, textColor }) {
+	try {
+		const circle = element.childNodes[1].childNodes[1];
+
+		// Lista de icones de +
+		const plusIconsIds = ["Caminho_187626", "Caminho_187624"];
+		const iconsIds = [plusIconsIds, "Caminho_187625"].flat();
+
+		// Limpa os filhos
+		const cleanNodes = () => {
+			element.childNodes.forEach((node, index) => {
+				if (iconsIds.includes(node?.id)) {
+					element.removeChild(element.childNodes[index]);
+				}
+			});
+		};
+
+		// Verifica se o icone é de + ou de -
+		const nodes = [...element.childNodes];
+		const isPlusIcon = nodes.some(item => plusIconsIds.includes(item?.id));
+		if (isPlusIcon) {
+			cleanNodes();
+			element.append(lessIcon.cloneNode(true));
+			circle.setAttribute("fill", "#7A2B58");
+		}
+		else {
+			cleanNodes();
+			element.append(plusIcon.cloneNode(true));
+			circle.setAttribute("fill", "#03B5B0");
+		}
+		bigCircle.setAttribute("style", `background: ${color}`);
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+leftCircle.element.addEventListener("click", () => changeIcon({ ...leftCircle }));
+topCircle.element.addEventListener("click", () => changeIcon({ ...topCircle }));
+rightCircle.element.addEventListener("click", () => changeIcon({ ...rightCircle }));
 
