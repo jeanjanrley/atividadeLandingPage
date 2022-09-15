ccb6ba9ed84b5b310dfe638669b9eb51e4c5d887
Jean Janrley <jeanjanrley@outlook.com>
2022-09-15 17:59:25 -0300

    Refatoração de modulos JS

---
+++ modules/cardsAnimator.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll(".drop-down");
+const cardsMoreInfo = document.querySelectorAll("#more-info");
+
+
+// Animação dos Card's
+buttons.forEach((element, index) => {
+	try {
+		element.addEventListener("click", () => {
+			// Veirifca se as informações estão escondidas ou exibidas
+			const result = cardsMoreInfo[index].classList.contains("more-info-area-hidden");
+
+			// Troca a classe da div more info para esconde ou mostrar e altera o estilo do botão
+			if (result) {
+				cardsMoreInfo[index].classList.replace("more-info-area-hidden", "more-info-area");
+				element.setAttribute("style", "background: #7A2B58");
+				element.childNodes[1].setAttribute("src", "/assets/icon-arrow-up.svg");
+			} else {
+				cardsMoreInfo[index].classList.replace("more-info-area", "more-info-area-hidden");
+				element.setAttribute("style", "background: #fff");
+				element.childNodes[1].setAttribute("src", "/assets/icon-arrow-down.svg");
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
--- modules/interactiveIllustration.js
+++ modules/interactiveIllustration.js
@@ -1,31 +1,3 @@
-const buttons = document.querySelectorAll(".drop-down");
-const cardsMoreInfo = document.querySelectorAll("#more-info");
-
-
-// Animação dos Card's
-buttons.forEach((element, index) => {
-	try {
-		element.addEventListener("click", () => {
-			// Veirifca se as informações estão escondidas ou exibidas
-			const result = cardsMoreInfo[index].classList.contains("more-info-area-hidden");
-
-			// Troca a classe da div more info para esconde ou mostrar e altera o estilo do botão
-			if (result) {
-				cardsMoreInfo[index].classList.replace("more-info-area-hidden", "more-info-area");
-				element.setAttribute("style", "background: #7A2B58");
-				element.childNodes[1].setAttribute("src", "./public/icones/icon-arrow-up.svg");
-			} else {
-				cardsMoreInfo[index].classList.replace("more-info-area", "more-info-area-hidden");
-				element.setAttribute("style", "background: #fff");
-				element.childNodes[1].setAttribute("src", "./public/icones/icon-arrow-down.svg");
-			}
-
-		});
-	} catch (error) {
-		console.log(error);
-	}
-});
-
 const bigCircle = document.querySelector("#big-circle");
 const interactiveMessage = document.querySelector("#big-circle > p");
 
@@ -37,12 +9,14 @@ const redCircle = {
 	color: "#FF3737",
 	message: "Texto referente ao botão que está sobre a flecha vermelha!",
 };
+
 const yellowCircle = {
 	id: "Grupo_126547",
 	element: document.getElementById("Grupo_126547"),
 	color: "#FFCC33",
 	message: "Este texto é referente ao botão que está sobre a flecha amarela!",
 };
+
 const greyCircle = {
 	id: "Grupo_126546",
 	element: document.getElementById("Grupo_126546"),
@@ -77,7 +51,6 @@ function changeIcon({ element, color, message }) {
 
 		// Resetar os botões para o estado positivo
 		circles.forEach(({ element: elementHere }) => {
-			console.log("element here: ", elementHere);
 			if (element?.id !== elementHere?.id) {
 				removeChilds(elementHere); // Remove todos os filhos do componente
 				elementHere.append(plusIcon.cloneNode(true)); // Adicionar o icone de  +
