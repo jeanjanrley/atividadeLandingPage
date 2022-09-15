c83e2ad5aae81d5e3d15a38c7cf6f6863571c79b
Jean Janrley <jeanjanrley@outlook.com>
2022-09-15 12:18:58 -0300

    Funcionalidade de esconde e mostrar mais informação implatada

---
+++ index.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll(".drop-down");
+const cardsMoreInfo = document.querySelectorAll("#more-info");
+
+buttons.forEach((element, index) => {
+	try {
+		element.addEventListener("click", () => {
+			// Veirifca se as informações estão escondidas ou exibidas
+			const result = cardsMoreInfo[index].classList.contains("more-info-area-hidden");
+
+			// Troca a classe da div more info para esconde ou mostrar
+			if (result) {
+				cardsMoreInfo[index].classList.replace("more-info-area-hidden", "more-info-area");
+				element.setAttribute("style", "background: #7A2B58");
+				element.childNodes[1].setAttribute("src", "./public/icones/icon-arrow-up.svg");
+			} else {
+				cardsMoreInfo[index].classList.replace("more-info-area", "more-info-area-hidden");
+				element.setAttribute("style", "background: #fff");
+				element.childNodes[1].setAttribute("src", "./public/icones/icon-arrow-down.svg");
+			}
+
+		});
+	} catch (error) {
+		console.log(error);
+	}
+});
