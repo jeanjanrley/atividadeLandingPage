f175eac54d353a1f927d994d31616972b682fbdc
Jean Janrley <jeanjanrley@outlook.com>
2022-09-16 19:04:01 -0300

    Correção de bug de acessibilidade nos botões dos cards

--- modules/cardsAnimator.js
+++ modules/cardsAnimator.js
@@ -19,14 +19,14 @@ buttons.forEach((element, index) => {
 				element.setAttribute("style", "background: #7A2B58");
 				element.childNodes[1].setAttribute("src", iconArrowUpURL);
 				element.childNodes[1].setAttribute("alt", "Icone totalmente verde claro de uma seta para cima");
-				cardsMoreInfo[index].setAttribute("alt", "Exibir menos");
+				element.setAttribute("alt", "Exibir menos");
 
 			} else {
 				cardsMoreInfo[index].classList.replace("more-info-area", "more-info-area-hidden");
 				element.setAttribute("style", "background: #fff");
 				element.childNodes[1].setAttribute("src", iconArrowDownURL);
 				element.childNodes[1].setAttribute("alt", "Icone totalmente branco de uma seta para baixo");
-				cardsMoreInfo[index].setAttribute("alt", "Exibir mais");
+				element.setAttribute("alt", "Exibir mais");
 			}
 		});
 	} catch (error) {
